996ab4af5290a400622556a5a64cc59ba2441bef
Joel Tannas <jtannas@gmail.com>
2024-01-20 13:31:02 -0700

    create dark mode toggle component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		fontFamily: {
 			sans: ["ubuntu", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
